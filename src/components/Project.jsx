317f946b7f570702c34f7a1e1d9aaef2306e90b0
zachspriggs <zachspriggs@gmail.com>
2024-09-17 20:06:24 -0700

    Updating projects. Instead of having indiviual cards, I'm going to a specific page to describe each project.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/styles/Projects.module.scss';
 
 const Project = ({ project }) => {
-  
+  const projectSlug = encodeURIComponent(project.title.toLowerCase().replace(/\s+/g, '-'));
+
   return (
-    <div>Project</div>
+    <Link 
+      href={`/projects/${projectSlug}`}
+      className={styles.projectLink}
+    >
+      <div className={styles.flipCard}>
+        <div className={styles.flipCardInner}>
+          <div className={styles.flipCardFront}>
+            <div className={styles.titleBar}>
+              <h3>{project.title}</h3>
+            </div>
+            <Image src={project.image} alt={project.title} width={500} height={300} className={styles.projectImage} />
+            <strong className={styles.elevatorPitch}>{project.elevator_pitch}</strong>
+            <div className={styles.teamSize}>
+              <strong>Time Line: {project.duration}</strong>
+              <h4>{project.team} Project</h4>
+              {project.team_size.map((member, index) => (
+                <p key={index}>{member}</p>
+              ))}
+            </div>
+            <div className={styles.technologiesGrid}>
+              {project.technologies.map((tech, index) => (
+                <span key={index} className={styles.techIcon}>{tech}</span>
+              ))}
+            </div>
+          </div>
+          <div className={styles.flipCardBack}>
+            <h3>{project.title}</h3>
+            <strong>{project.description}</strong>
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
 
