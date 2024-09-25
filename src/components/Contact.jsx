1aef93e35976e184d2ba9a1fc8207319743aff2b
Zach Spriggs <zachspriggs@gmail.com>
2024-09-25 16:03:48 -0700

    adjusting contact component, styling adjustments for mobile, starting on project page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,19 +16,11 @@ export default function Contact() {
           </a>
         </div>
         <div className={styles.contactCard}>
-          <FaGithub size={50} style={{ marginBottom: "10px" }} />
+          <FaGithub size={50} style={{ marginBottom: "10px"}} />
           <a href="https://github.com/ZachSpriggs" target="_blank">
             GitHub
           </a>
         </div>
-        {/* <div className={styles.contactCard}>
-          <MdEmail size={50} style={{ marginBottom: "10px" }} />
-          <a href= "mailto: zachspriggs@gmail.com">zachspriggs@gmail.com</a>
-        </div>
-        <div className={styles.contactCard}>
-          <FaPhone size={40} style={{ marginBottom: "10px" }} />
-          <a href="tel:360-477-2535">(360) 477-2535</a>
-        </div> */}
       </div>
     </div>
   );
