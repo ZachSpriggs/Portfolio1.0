2ecda3d10d2adf07b8ab822f218816429910866c
Zach Spriggs <zachspriggs@gmail.com>
2024-09-27 12:18:13 -0700

    Adjusting project descriptions and finalizing frontend design

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,45 +1,39 @@
-import React from 'react';
-import styles from '@/styles/Project.module.scss';
-
+import React from "react";
+import styles from "@/styles/Project.module.scss";
 
 const Project = ({ project }) => {
   console.log("Rendering Project component, project:", project);
-  
+
   if (!project) {
     return <div>Project data not available</div>;
   }
-
 
   return (
     <div className={styles.projectContainer}>
       <h1 className={styles.projectTitle}>{project.title}</h1>
       <p className={styles.elevatorPitch}>{project.elevator_pitch}</p>
-      
-      <div className={styles.projectDetails}>
-        <div className={styles.detailItem}>
-          <h3>Duration:</h3>
-          <p>{project.duration}</p>
-        </div>
-        <div className={styles.detailItem}>
-          <h3>Team Size:</h3>
-          <ul>
-            {project.team_size.map((member, index) => (
-              <li key={index}>{member}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+
       <h3>Technologies Used:</h3>
       <div className={styles.technologiesGrid}>
-        <ul>
-          {project.technologies.map((tech, index) => (
-            <li key={index} className={styles.techIcon}>{tech}</li>
-          ))}
-        </ul>
+        {project.tech_stack.map((tech, index) => (
+          <span key={index} className={styles.techIcon}>
+            {tech}
+          </span>
+        ))}
       </div>
-
+      <hr />
       <h3>Project Description:</h3>
       <p className={styles.projectDescription}>{project.description}</p>
+      <hr />
+      <h3>Pain Point:</h3>
+      <p className={styles.projectDescription}>{project.pain_point}</p>
+      <hr />
+      <h3>Solution:</h3>
+      <p className={styles.projectDescription}>{project.solution}</p>
+      <hr />
+      <h3>Key Takeaway:</h3>
+      <p className={styles.projectDescription}>{project.takeaway}</p>
+      <hr />
     </div>
   );
 };
