a0b0fdcaf4178b4e6ba42d11dd1fe07d40c629c0
Zach Spriggs <zachspriggs@gmail.com>
2024-10-09 12:29:50 -0700

    put gtag in env variable

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,16 @@
 import { Montserrat } from "next/font/google";
 import ClientLayout from "./ClientLayout";
-import '@/styles/globals.scss';
-import { Metadata } from 'next';
-import Script from 'next/script';
+import "@/styles/globals.scss";
+import { Metadata } from "next";
+import Script from "next/script";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   icons: {
     icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
   },
 };
@@ -21,18 +21,18 @@ export default function RootLayout({ children }) {
       <head>
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-B1ZC50VBYG`}
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
         <Script
           id="gtag-init"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-B1ZC50VBYG');
-            `,
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+    `,
           }}
         />
       </head>
