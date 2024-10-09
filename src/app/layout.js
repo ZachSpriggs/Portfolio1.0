0c6bc2a1171225edd1bea48f6cd75b26bd2f40bf
Zach Spriggs <zachspriggs@gmail.com>
2024-10-09 12:26:14 -0700

    added google analytics for better insights into profile views

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Montserrat } from "next/font/google";
 import ClientLayout from "./ClientLayout";
 import '@/styles/globals.scss';
 import { Metadata } from 'next';
+import Script from 'next/script';
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -17,6 +18,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={montserrat.className}>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-B1ZC50VBYG`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B1ZC50VBYG');
+            `,
+          }}
+        />
+      </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
