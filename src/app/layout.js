77a7b074c40b199a9f374b47a5e8d1af32065f89
Zach Spriggs <zachspriggs@gmail.com>
2024-10-09 11:35:34 -0700

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,18 @@
 import { Montserrat } from "next/font/google";
 import ClientLayout from "./ClientLayout";
 import '@/styles/globals.scss';
-import Script from 'next/script';
+import { Metadata } from 'next';
 
 const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata = {
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
